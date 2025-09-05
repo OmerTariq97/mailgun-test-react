5c5eee9a61eb882044325f722d8ae45add316bb7
Omer Tariq <omer.tariq@sparkai.ca>
2025-09-05 17:49:47 +0500

    Revert "helo"
    This reverts commit eee9c35e83ee5cbd1168de070b701c201673d09c.

--- mailgun test react/mailgun_webhook/mailgun_webhook/app.py
+++ mailgun test react/mailgun_webhook/mailgun_webhook/app.py
@@ -20,7 +20,6 @@ class Mailgun(db.Model):
 def mailgun_recieve_email():
     payload = request.data
     payload_parsed = json.loads(payload)
-    print('helo')
 
     email = payload_parsed['event-data']['recipient']
     subject = payload_parsed['event-data']['message']['headers']['subject']
